ebf1395ef1d96094eef0e59424bc41de45c1fd12
Drew Whitmer <aswhitme@ucsc.edu>
2025-11-06 11:50:50 -0800

    actual final website prototype with final build

--- Prototype_Website/CMPM170Prototype_Website/Assets/Scripts/Scrubbing.cs
+++ Prototype_Website/CMPM170Prototype_Website/Assets/Scripts/Scrubbing.cs
@@ -21,30 +21,25 @@ public class Scrubbing : MonoBehaviour
     void Update()
     {
 
-        if (!Input.GetMouseButton(0))
-        {
-            return; //returns if mouse is not pressed
-        }
-
         //detects intersecting character for body text, sets characters intersecting with mouse to ' ', adds to progress bar.
         char[] newText1 = body.text.ToCharArray();
         int a = TMP_TextUtilities.FindIntersectingCharacter(body, Input.mousePosition, Camera.current, true);
-        if (a != -1 && newText1[a] != ' ')
+        if (a != -1 && newText1[a] != ' ' && Input.GetMouseButton(0))
         {
             newText1[a] = ' ';
-            body.SetText(newText1);
             bar.value++;
         }
+        body.SetText(newText1);
 
         //same thing but with the header.
         char[] newText2 = header.text.ToCharArray();
         int b = TMP_TextUtilities.FindIntersectingCharacter(header, Input.mousePosition, Camera.current, true);
-        if (b != -1 && newText2[b] != ' ')
+        if (b != -1 && newText2[b] != ' ' && Input.GetMouseButton(0))
         {
             newText2[b] = ' ';
-            header.SetText(newText2);
             bar.value++;
         }
+        header.SetText(newText2);
 
         if (bar.value == bar.maxValue)
         {
